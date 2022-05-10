3522dcd79efa0d8ec190c1b45b20c6e9919780ae
ffrancacorrea <ffrancacorrea@gmail.com>
2022-05-10 18:36:04 +0200

    fix font

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,45 +2,49 @@ import React from "react";
 import styled from "@emotion/styled";
 import coverHouse from "../../assets/coverHouse.jpg";
 
-// TODO check fonts
 const Wrapper = styled.div`
-  @import url("https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap");
   display: flex;
   flex-direction: column;
-  background-image: url(${coverHouse});
+  background-image: linear-gradient(
+      to left bottom,
+      rgba(0, 0, 0, 0.6),
+      rgba(0, 0, 0, 0.2)
+    ),
+    url(${coverHouse});
   margin: 0;
   width: 100%;
   height: 100vh;
   background-repeat: no-repeat;
+  background-size: cover;
 `;
 const Content = styled.div`
   display: flex;
   justify-content: center;
-  line-height: 100vh;
+  padding-top: 40vh;
   text-align: center;
   color: white;
   h1 {
     font-family: "Dancing Script", cursive;
-    font-weight: normal;
+    font-weight: bold;
     font-style: normal;
+    font-size: 500%;
   }
 `;
-const NavBar = styled.div``;
 const Card = styled.div`
   display: flex;
   align-self: center;
   justify-content: center;
-  background-color: #303540;
+  background-color: #1d2024;
   width: 30%;
-  height: 120px;
-  margin-top: -10vh;
+  cursor: pointer;
+  margin-top: 41vh;
   border-radius: 2px;
   box-shadow: 6px 7px 15px -2px rgba(0, 0, 0, 0.75);
   h3 {
     text-align: center;
     color: white;
-    margin-top: 60px;
-    font-family: "Dancing Script", cursive;
+    padding: 35px;
+    font-family: "Dancing Script", cursive; // TODO change font
     font-weight: normal;
     font-style: normal;
   }
@@ -49,7 +53,6 @@ const Card = styled.div`
 const Header = () => {
   return (
     <Wrapper>
-      <NavBar></NavBar>
       <Content>
         <h1>Wonderful Houses Gallery</h1>
       </Content>
